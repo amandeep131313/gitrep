a1f3f34ff57db20b5090a4aac9e932c221577f44
AzureAD\AmanDeep <aman.deep1@metlife.com>
2019-07-22 11:48:03 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import mock from './mock.json';
+import ProductDescription from './ProductDescription';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      mock:mock,
+      clickBuy:0
+    }
+  }
+  setclickOnBuy=(index)=>{
+    let oldIndex = this.state.clickBuy;
+    this.setState({
+      clickBuy:oldIndex+1
+    })
+  }
+        render(){
+          let rows = []
+          this.state.mock.forEach((data)=>{
+              rows.push(<ProductDescription setclickOnBuy={this.setclickOnBuy} data={data}/>)
+          })
+          console.log(this.state.mock)
+        return (
+          <div className="container">
+            <div className="Shopping-title text-center">
+              <h2> Welcome to Shopping Cart </h2>
+              </div>
+            <div className="shopping-card">
+            <div className="cart-info text-right">
+                <h2>Total Item = {this.state.clickBuy} </h2>
+            </div>
+            <div className="product-detail">
+              {rows}
+            </div>
+            </div>  
+          </div>
+        );
+      }
+
 }
-
 export default App;
---
+++ src/ProductDescription.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Modal from 'react-responsive-modal';
+class  ProductDescription extends React.Component {
+    constructor(props){
+        super(props);
+            this.state={
+             show:false
+            }
+            this.description = ''
+        }
+        showModal = (e,desc)=>{
+          this.setState({
+            show:true
+          });
+          this.description = desc
+        } 
+        clickOnBuy=() =>{
+          console.log('index')
+            this.props.setclickOnBuy(1);
+            this.setState({
+              show:false
+            });
+        }
+        onCloseModal=()=>{
+          this.setState({
+            show:false
+          });
+        }
+        render(){
+            let rows = [];
+            this.props.data.items.forEach((productinfo)=>{
+                rows.push(
+                        <div className="col-md-4">
+                        <div class="card" onClick={e=>this.showModal(e,productinfo.description)} >
+                                     <div class="card-body">
+                                   <div className="card-body-model text-center">
+                                      <h2>{productinfo.model}</h2>
+                                   </div> 
+                                   <div className="card-body-model text-center">
+                                      <h2>{productinfo.price} </h2>
+                                   </div>  
+                                   <div className="card-description">
+                                      <input onClick={e=>this.showModal(e,productinfo.description)} className="form-control primary-button" value="Show description" type="button"></input>
+                                   </div>
+                                   <div className="card-description">
+                                      <input className="form-control primary-button" onClick={e=>this.clickOnBuy(e)} value="Buy" type="button"></input>
+                                   </div>
+                                     </div>
+                </div>
+                             
+                        </div>
+                )
+            });
+        return (
+          <div className="model-description">
+            <div className="type-of-model">
+               <h2> {this.props.data.product} </h2>
+            </div>
+            <div className="row">
+               {rows}
+            </div>
+          <Modal id="showmodal" center classNames={{top:'50px',width:'60px'}} open={this.state.show} onClose={this.onCloseModal}>
+            <div className="card-description">
+              <h1>{this.description}</h1>
+             </div> 
+              <div className="card-description">
+                <input className="form-control primary-button" onClick={e=>this.clickOnBuy(e)} value="Buy" type="button"></input>
+                </div>
+         </Modal>
+          </div>
+        );
+      }
+
+}
+export default ProductDescription;
